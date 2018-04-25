8dc7b9c0d6e865a99c733ff374e426e577226784
crisciano <criscianotsi@gmail.com>
2018-04-25 11:28:16 -0300

    Primeira versão...

---
+++ README.md
@@ -0,0 +1,29 @@
+# Feed wordpress
+
+O script tem como objetivo exibir no site o feed do wordpress.
+Dev: Crisciano S. Botelho.
+
+Foi desenvolvido com:
+
+`Gulp`
+`Sass`
+`Watch`
+`Server `
+`TinyPng`
+`Swiper`
+`Bootstrap 4`
+`Font Awesome`
+`FlexBox`
+
+Para iniciar e preciso ter previamente instalado o nodejs.
+Instalar as bibliotecas do node.
+
+`npm install`
+
+Utilizar o gulp para rodar o projeto
+
+`gulp server`
+
+Para gerar o build a versão compactada.
+
+`gulp`
---
+++ src/php/feed-wd.php
@@ -0,0 +1,27 @@
+<?php
+// config header
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=utf-8');
+//Include WordPress
+define('WP_USE_THEMES', false);
+require('../blog/wp-load.php');
+
+//Define quantos posts serão exibidos
+query_posts('showposts=3');
+// while para percorrer os posts se houver
+while (have_posts()): the_post();  
+	// get img $post = null e tamanho medium pode ser 'thumbnail', 'medium', 'large', 'full'
+	$obj['img'] 		= get_the_post_thumbnail_url(null, 'medium'); 
+	// get title do post
+	$obj['title'] 		= get_the_title();
+	// get description do post
+	$obj['description'] = get_the_excerpt();
+	// get url referente ao post no blog
+	$obj['url'] 		= get_the_permalink();
+	// inclui o objt no data
+	$data[] 			= $obj;
+// fim do while
+endwhile;
+// transforma o data em um json
+echo json_encode($data);
+?>
